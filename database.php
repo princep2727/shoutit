f3d40e945485df38debef988846c5f2ec1f93158
princep2727 <59334199+princep2727@users.noreply.github.com>
2020-06-27 00:45:05 +0530

    first commit

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+//Connect to MySQL
+$con = mysqli_connect("localhost","root","jack007","shouit");
+
+//Test Connection
+if(mysqli_connect_errno()){
+	echo 'Failed to connect to MySQL: '.mysqli_connect_error();
+}
